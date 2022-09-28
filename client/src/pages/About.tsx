a43144bb0cfe353afa824174a8658a5b739a7539
Nick <nicolastiennguyen@gmail.com>
2022-09-28 14:54:35 -0700

    added description

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export function About() {
-  return <h1>About</h1>
+  return (
+    <>
+      <h1>About</h1>
+      <p>Good Eats! helps you answer the dreaded question of "where should we eat tonight?" by randomly generating restaurants located near you.</p>
+    </>
+  )
 }
