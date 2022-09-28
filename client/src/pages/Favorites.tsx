64cad060957d1b6db4795e7647771dbd17fbc857
Nick <nicolastiennguyen@gmail.com>
2022-09-27 21:21:56 -0700

    front end setup

---
+++ client/src/pages/Favorites.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function Favorites() {
+  return <h1>Favorites</h1>
+}
---
+++ client/src/pages/Restaurants.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function Restaurants() {
+  return <h1>Restaurants</h1>
+}
