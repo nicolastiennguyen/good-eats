66369fe27d71895b1e94cf8128637e2c1106a745
Nick <nicolastiennguyen@gmail.com>
2022-09-28 14:55:08 -0700

    added description

--- client/src/pages/Restaurants.tsx
+++ client/src/pages/Restaurants.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export function Restaurants() {
-  return <h1>Restaurants</h1>
+  return (
+    <>
+      <h1>Restaurants</h1>
+      <p>This could be a list of all the restaurants near you?</p>
+    </>
+  )
 }
