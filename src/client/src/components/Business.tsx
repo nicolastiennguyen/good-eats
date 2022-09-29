19912328c6fe323ef1b75c9be20b75dc42dd9b7e
Nick <nicolastiennguyen@gmail.com>
2022-09-28 23:08:39 -0700

    modified information

--- src/client/src/components/Business.tsx
+++ src/client/src/components/Business.tsx
@@ -30,27 +30,18 @@ type display_address = {
 export default function Business({ id, name, image_url, url, rating, price, location }: BusinessProps) {
   return (
     <Card className="h-100">
-      <Card.Img className="center-block" variant="top" src={image_url} onClick={() => window.location.href=url} height="600px" style={{ objectFit: "cover", cursor: "pointer" }} />
-      <Card.Body className="d-flex flex-column">
-        <Card.Title className="align-items-baseline mb-4">
-          <span className="fs-2">{name}</span>
-          <span className="ms-2 text-muted">{
-            location ? `${location.display_address[0]} ${location.display_address[1]}` : null
-          }</span>
-          <span className="ms-2 text-muted">{rating}</span>
-          <span className="ms-2 text-muted">{price}</span>
+      <Card.Img className="center-block" variant="top" src={image_url} onClick={() => window.location.href=url} height="500px" style={{ objectFit: "cover", cursor: "pointer" }} />
+      <Card.Body>
+        <Card.Title>
+          <div className="fs-3">{name}</div>
+          <div className="d-flex">
+            <div className="ms-1 text-muted">{
+              location ? `${location.display_address[0]} ${location.display_address[1]}` : null
+            }</div>
+          </div>
+            <div className="ms-1 text-muted">Rating: {rating}/5</div>
+            <div className="ms-1 text-muted">{price}</div>
         </Card.Title>
-        {/* <div className="mt-auto">
-          {addedToFavorites ? (
-            <Button className="w-100" onClick={() => (setAddedToFavorites(false))}>+ Add to Favorites</Button>
-          )
-          : <div className="d-flex align-items-center flex-column" style={{ gap: ".5rem" }}>
-              <div className="d-flex align-items-center justify-content-center">
-                Added to Favorites!
-              </div>
-              <Button variant="danger" onClick={() => (setAddedToFavorites(true))}size="sm">Remove</Button>
-            </div>}
-        </div> */}
       </Card.Body>
     </Card>
   )
