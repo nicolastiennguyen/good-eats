3c2bb5fb32348377555329cf82d79c551c7ff561
Nick <nicolastiennguyen@gmail.com>
2022-09-28 23:08:20 -0700

    added Saved NavLink

--- src/client/src/components/Navbar.tsx
+++ src/client/src/components/Navbar.tsx
@@ -4,11 +4,12 @@ import { NavLink } from 'react-router-dom'
 
 export function Navbar() {
   return (
-  <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+  <NavbarBs sticky="top" className="shadow-lg p-3 mb-5 bg-white rounded">
     <Container>
       <Nav className="me-auto">
         <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
         <Nav.Link to="/favorites" as={NavLink}>Favorites</Nav.Link>
+        <Nav.Link to="/saved" as={NavLink}>Saved</Nav.Link>
         <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
       </Nav>
       Good Eats!
