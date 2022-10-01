0f7520c9df6b822ca5c7b3015be207633556d3b5
Nick <nicolastiennguyen@gmail.com>
2022-09-30 21:04:05 -0700

    compiled ts

--- build/client/src/components/Business.js
+++ build/client/src/components/Business.js
@@ -2,10 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
 const react_bootstrap_1 = require("react-bootstrap");
-function Business({ name, image_url, url, rating, price }) {
-    let quantity = false;
-    return ((0, jsx_runtime_1.jsxs)(react_bootstrap_1.Card, { className: "h-100", children: [(0, jsx_runtime_1.jsx)(react_bootstrap_1.Card.Img, { variant: "top", src: image_url, height: "200px", style: { objectFit: "cover" } }), (0, jsx_runtime_1.jsxs)(react_bootstrap_1.Card.Body, { className: "d-flex flex-column", children: [(0, jsx_runtime_1.jsxs)(react_bootstrap_1.Card.Title, { className: "d-flex justify-content-between align-items-baseline mb-4", children: [(0, jsx_runtime_1.jsx)("span", { className: "fs-2", children: name }), (0, jsx_runtime_1.jsx)("span", { className: "ms-2 text-muted", children: price })] }), (0, jsx_runtime_1.jsx)("div", { className: "mt-auto", children: quantity === false ?
-                            (0, jsx_runtime_1.jsx)(react_bootstrap_1.Button, { className: "w-100", onClick: () => quantity = true, children: "+ Add to Favorites" })
-                            : (0, jsx_runtime_1.jsx)(react_bootstrap_1.Button, { className: "w-100", children: "Added!" }) })] })] }));
+function Business({ id, name, image_url, url, rating, price, location }) {
+    return ((0, jsx_runtime_1.jsxs)(react_bootstrap_1.Card, Object.assign({ className: "h-100" }, { children: [(0, jsx_runtime_1.jsx)(react_bootstrap_1.Card.Img, { className: "center-block", variant: "top", src: image_url, onClick: () => window.location.href = url, height: "500px", style: { objectFit: "cover", cursor: "pointer" } }), (0, jsx_runtime_1.jsx)(react_bootstrap_1.Card.Body, { children: (0, jsx_runtime_1.jsxs)(react_bootstrap_1.Card.Title, { children: [(0, jsx_runtime_1.jsx)("div", Object.assign({ className: "fs-3" }, { children: name })), (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "d-flex" }, { children: (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "ms-1 text-muted" }, { children: location ? location.display_address ? `${location.display_address[0]} ${location.display_address[1]}` : `${location}` : null })) })), (0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "ms-1 text-muted" }, { children: ["Rating: ", rating, "/5"] })), (0, jsx_runtime_1.jsx)("div", Object.assign({ className: "ms-1 text-muted" }, { children: price }))] }) })] })));
 }
 exports.default = Business;
--- build/server/app.js
+++ build/server/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,7 +16,72 @@ const express_1 = __importDefault(require("express"));
 require('dotenv').config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const routes_1 = __importDefault(require("./routes"));
-app.use(routes_1.default);
+app.use(express_1.default.urlencoded({ extended: false }));
+const axios_1 = __importDefault(require("axios"));
+const { Client } = require('pg');
+const client = new Client({
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE
+});
+connect();
+function connect() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield client.connect();
+            console.log('connected');
+        }
+        catch (e) {
+            console.error(e);
+        }
+    });
+}
+// app.use(router)
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, DELETE, GET");
+    next();
+});
+app.get("/yelp/:location/:limit", (req, res) => {
+    axios_1.default
+        .get(`https://api.yelp.com/v3/businesses/search`, {
+        headers: {
+            Authorization: `Bearer ${process.env.YELP_API_TOKEN}`,
+        },
+        params: {
+            location: req.params.location,
+            limit: req.params.limit
+        }
+    })
+        .then(response => {
+        res.send(response.data);
+    })
+        .catch(error => {
+        console.log('error');
+    });
+});
+app.get("/businesses", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const results = yield client.query("select * from businesses");
+        res.json(results.rows);
+    }
+    catch (e) {
+        console.log(e);
+    }
+}));
+app.post("/businesses", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id, name, image_url, url, rating, price, location } = req.body;
+    try {
+        yield client.query("INSERT INTO businesses VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT (id) DO NOTHING", [id, name, image_url, url, rating, price, location]);
+        console.log('inserted into database');
+    }
+    catch (e) {
+        console.log(e);
+    }
+}));
 let PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
