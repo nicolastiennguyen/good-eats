8191ad1cb5933d23181031debf7fbe962c8bf0b0
Nick <nicolastiennguyen@gmail.com>
2022-10-06 19:23:07 -0700

    changed p to h5

--- src/client/src/pages/About.tsx
+++ src/client/src/pages/About.tsx
@@ -4,7 +4,7 @@ export function About() {
   return (
     <>
       <h1>About</h1>
-      <p>Good Eats! helps you answer the dreaded question of "where should we eat?" by randomly generating restaurants located near you.</p>
+      <h5>Good Eats! helps you answer the dreaded question of "where should we eat?" by randomly generating restaurants located near you.</h5>
     </>
   )
 }
