3754ee8cc67de56a61257d81c7e2085663574280
Nick <nicolastiennguyen@gmail.com>
2022-10-14 09:52:16 -0700

    updated imports

--- src/client/src/pages/Saved.tsx
+++ src/client/src/pages/Saved.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Business from '../components/Business';
 
