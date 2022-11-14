4044733eade7602caf4adcf70813214fd2bebd1d
Nicolas Nguyen <nicolastiennguyen@gmail.com>
2022-11-13 21:33:26 -1000

    updated formatting of imports

--- client/src/pages/Saved.tsx
+++ client/src/pages/Saved.tsx
@@ -1,7 +1,7 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Row, Col } from 'react-bootstrap';
 import Business from '../components/Business';
+import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 export function Saved() {
   const [saved, setSaved] = useState([]);
