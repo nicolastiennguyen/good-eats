f0e772a96ef0a6e25667b15ec08bb3e6c0f007fd
Nicolas Nguyen <nicolastiennguyen@gmail.com>
2022-11-14 20:28:54 -1000

    updated formatting of App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import { Home } from './pages/Home';
 import { Favorites } from './pages/Favorites';
-import { Saved } from './pages/Saved';
-import { About } from './pages/About';
 import { Navbar } from './components/Navbar';
+import { Container } from 'react-bootstrap';
+import { About } from './pages/About';
+import { Saved } from './pages/Saved';
+import { Home } from './pages/Home';
 
 function App() {
   return (
